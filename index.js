2f7ed100f4a34a035d50bc7654b80748256ca76b
shifachaus <chausshifa804@gmail.com>
2024-01-22 15:39:27 +0530

    feat: add blog creation and listing

--- index.js
+++ index.js
@@ -2,8 +2,8 @@ const path = require("path");
 const express = require("express");
 const mongoose = require("mongoose");
 const cookieParser = require("cookie-parser");
+const Blog = require("./models/blog");
 
-const userRoutes = require("./routes/user");
 const {
   checkForAuthenticationCookie,
 } = require("./middlewares/authentication");
@@ -11,24 +11,40 @@ const {
 const app = express();
 const PORT = 8000;
 
+//Routes
+const userRoute = require("./routes/user");
+const blogRoute = require("./routes/blog");
+
+//DB Connections
 mongoose
   .connect("mongodb://127.0.0.1:27017/blogify")
   .then(() => console.log("MongoDB Connected !"));
 
+//EJS
 app.set("view engine", "ejs");
 app.set("views", path.resolve("./views"));
 
+//Middlewares
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(checkForAuthenticationCookie("token"));
+app.use(express.static(path.resolve("./public")));
 
-app.get("/", (req, res) => {
-  return res.render("home", {
-    user: req.user,
-  });
+app.get("/", async (req, res) => {
+  try {
+    const allBlogs = await Blog.find({}).sort({ createdAt: -1 });
+    return res.render("home", {
+      user: req.user,
+      blogs: allBlogs,
+    });
+  } catch (error) {
+    console.error(error);
+    return res.status(500).send("Internal Server Error");
+  }
 });
 
-app.use("/user", userRoutes);
+app.use("/user", userRoute);
+app.use("/blog", blogRoute);
 
 app.listen(PORT, console.log(`Server Started at PORT ${PORT}`));
---
+++ routes/blog.js
@@ -0,0 +1,47 @@
+const express = require("express");
+const Blog = require("../models/blog");
+const multer = require("multer");
+const path = require("path");
+
+const router = express.Router();
+
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, path.resolve(`./public/uploads`));
+  },
+  filename: function (req, file, cb) {
+    const fileName = `${Date.now()}-${file.originalname}`;
+    cb(null, fileName);
+  },
+});
+
+const upload = multer({ storage: storage });
+
+router.get("/add-new", (req, res) => {
+  return res.render("addBlog", {
+    user: req.user,
+  });
+});
+
+router.get("/:id", async (req, res) => {
+  const blog = await Blog.findById(req.params.id);
+
+  return res.render("blog", {
+    user: req.user,
+    blog,
+  });
+});
+
+router.post("/", upload.single("coverImageURL"), async (req, res) => {
+  const { title, body } = req.body;
+  const blog = await Blog.create({
+    title,
+    body,
+    createdBy: req.user._id,
+    coverImageURL: `/uploads/${req.file.filename}`,
+  });
+
+  return res.redirect(`/blog/${blog._id}`);
+});
+
+module.exports = router;
